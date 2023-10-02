09445c310eab1491a4aa625161701016885b1892
Gabriel Leal Felix <75867684+GabrielLealFelix12@users.noreply.github.com>
2023-10-02 17:27:07 -0400

    Add files via upload

--- app/loja/components_loja/searchbar.tsx
+++ app/loja/components_loja/searchbar.tsx
@@ -3,10 +3,10 @@
 function Searchbar(): React.JSX.Element {
 return(
     <>
-    <div style={{height:'25px'}}> </div>
+    <div style={{height:'25px'}} className="bg-indigo-400"> </div>
 
-    <div className="flex flex-row flex-wrap ml-6">
-    <input type="text" className="rounded-l-lg"/>
+    <div className="flex flex-row flex-wrap ml-6 bg-indigo-400">
+    <input type="text" className="rounded-l-lg max-w-[50%]"/>
     <button className="bg-indigo-200 text-center w-32 rounded-r-lg"> Pesquisar </button>
     </div>
     </>
