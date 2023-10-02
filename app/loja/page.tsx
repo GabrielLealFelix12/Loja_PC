3568a1bfb137db82fb647ebd49a242d92b4c3e82
Gabriel Leal Felix <75867684+GabrielLealFelix12@users.noreply.github.com>
2023-10-02 17:29:39 -0400

    nova page layout
    nova page layout

--- app/loja/page.tsx
+++ app/loja/page.tsx
@@ -1,16 +1,28 @@
 import { Metadata } from 'next'
 import { Headerbarloja } from "./components_loja/headerbarloja"
 import { Sidebar } from './components_loja/sidebar_filter'
+import { RenderproductcardLoja } from './components_loja/product_loja'
 
 export const metadata: Metadata = {
     title: 'Loja'
   }
 
   export default function Page() {
+    
 return(
     <>
-    <Headerbarloja/>
-    <Sidebar/>
+    <Headerbarloja/>    
+    <div className='w-full h-screen flex flex-row flex-wrap bg-indigo-400'>
+     
+    <div className='w-1/4 bg-indigo-400'> 
+      <Sidebar/>
+    </div>
+    
+    <div className='w-3/4 bg-indigo-400'>
+      <RenderproductcardLoja/>
+    </div>
+    
+    </div>
     <link rel="stylesheet" href="/stylesheet.css"/> 
     </>
 )
